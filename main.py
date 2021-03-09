e3047cf8a5983348808f043d6b5acc8b9bf78cb4
Disha Sharma <dsharma15@hawk.iit.edu>
2021-03-08 19:40:13 -0600

    image segmentation module development

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import numpy as np
+from skimage.color import rgb2gray
+import cv2 
+import matplotlib.pyplot as plt 
+from scipy import ndimage
+# %matplotlib inline
+
+image = plt.imread('1.jpeg')
+image.shape
+plt.imshow(image)
