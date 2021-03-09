8972d9f4df6423c7b041bfacfb1b5802a6c464c0
Disha Sharma <dsharma15@hawk.iit.edu>
2021-03-08 20:00:11 -0600

    region based semantic detection module development

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy import ndimage
 # %matplotlib inline
 
-image = plt.imread('1.jpeg')
+image = plt.imread('1.jpg')
 print(image.shape)
 print(plt.imshow(image))
 
@@ -13,3 +13,13 @@ gray = rgb2gray(image)
 print(plt.imshow(gray, cmap='gray'))
 
 print(gray.shape)
+
+gray_r = gray.reshape(gray.shape[0]*gray.shape[1])
+for i in range(gray_r.shape[0]):
+	if gray_r[i] > gray_r.mean():
+		gray_r[i] = 1
+	else:
+		gray_r[i] = 0
+
+gray = gray_r.reshape(gray.shape[0], gray.shape[1])
+print(plt.imshow(gray, cmap='gray'))
